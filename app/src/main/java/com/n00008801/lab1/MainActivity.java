bdf0bf99f74ad5e14610422f56f93f2eb53f1e10
Chris Burgener <n00008801@humbermail.ca>
2020-09-25 02:00:01 -0400

    Implement Activity files, add layout files

--- app/src/main/java/com/n00008801/lab1/MainActivity.java
+++ app/src/main/java/com/n00008801/lab1/MainActivity.java
@@ -6,9 +6,49 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextFragment textFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textFragment = (TextFragment) getSupportFragmentManager().findFragmentById(R.id.text_fragment);
+        textFragment.updateTextView(getResources().getString(R.string.oncreate));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        textFragment.updateTextView(getResources().getString(R.string.onstart));
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        textFragment.updateTextView(getResources().getString(R.string.onrestart));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        textFragment.updateTextView(getResources().getString(R.string.onresume));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        textFragment.updateTextView(getResources().getString(R.string.onpause));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        textFragment.updateTextView(getResources().getString(R.string.ondestroy));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        textFragment.updateTextView(getResources().getString(R.string.onstop));
     }
 }
--- app/src/main/java/com/n00008801/lab1/VRActivity.java
+++ app/src/main/java/com/n00008801/lab1/VRActivity.java
@@ -1,4 +1,42 @@
 package com.n00008801.lab1;
 
-public class VRActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class VRActivity extends AppCompatActivity {
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vractivity);
+        tv = (TextView)findViewById(R.id.txtView_vractivity);
+        updateTextView(getResources().getString(R.string.vractivity));
+        updateTextView(getResources().getString(R.string.oncreate));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        updateTextView(getResources().getString(R.string.onstart));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        updateTextView(getResources().getString(R.string.onstop));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        updateTextView(getResources().getString(R.string.ondestroy));
+    }
+
+    public void updateTextView(String text) {
+        tv.append(text + "\n");
+    }
 }
